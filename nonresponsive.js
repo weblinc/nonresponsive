3f99e6a688d6f73a839aa1d8ce10708af14bcb6e
dknight <dknight@weblinc.com>
2012-12-03 21:00:32 -0500

    Initial commit

---
+++ nonresponsive.js
@@ -0,0 +1,89 @@
+/* Nonresponsive - Media queries for the unsupportive. Authors & copyright (c) 2012: WebLinc, David Knight. */
+/* NOTE: Depends on Media object. See https://github.com/weblinc/media-match */
+
+// Nonresponsive
+(function(win, source) {
+    // Check if we need to support this browser
+    if (Media.supported) {
+        win.Nonresponsive = {
+            parse: function() {},
+            apply: function() {}
+        };
+
+        return;
+    }
+
+    var _doc         = win.document,
+        _style       = _doc.createElement('style'),
+        _queryList   = [],
+        _rulesList   = [], 
+        _appliedCss  = '',
+        _cssText     = '',
+        _timer       = 0;
+
+    _style.type  = "text/css";
+    _style.id    = 'Nonresponsive';
+    _doc.getElementsByTagName('head')[0].appendChild(_style);
+
+    win.Nonresponsive = {
+        /*
+            parse
+            Finds all @media blocks and creates a list to be evaluated.
+            @param data <String>
+            
+                Input   : @media screen and (min-width: 300px) and (max-width: 600px), screen and (max-width: 1200px) { css rules }
+                Output  : 
+                            _queryList[screen and (min-width: 300px) and (max-width: 600px), screen and (max-width: 1200px)]
+                            _rulesList[css rules]
+         */
+        parse: function(data, apply) {
+            // Find all '@media type and (query) and (query), type and (query) { css rules }'
+            data.replace(/@media\s*([^\{]+)\{(([^\{\}]*\{[^\}\{]*\})+)?\}/gi, function(block, mql, rules) {
+                if (mql && rules) {
+                    _queryList.push(mql);
+                    _rulesList.push(rules);
+                }
+            });
+
+            apply && this.apply();
+        },
+
+        /*
+            apply
+         */
+        apply: function() {
+            clearTimeout(_timer);
+
+            _timer = setTimeout(function() {
+                // Updated supported media query features
+                Media.setMutableFeatures();
+                _cssText = '';
+
+                // Loop over query list and evaluate each query then build @media string
+                for (var i = 0, il = _queryList.length; i < il; i++) {
+                    var match = false;
+                    if ((match = Media.parseMatch(_queryList[i], true))) {
+                        _cssText += '@media ' + match.type.name + ' {' + _rulesList[i] + '}\n';
+                    }
+                }
+
+                // Add style node if css is new
+                if (_cssText != _appliedCss) {
+                    if (_style.styleSheet) {
+                        _style.styleSheet.cssText = _cssText;
+                    } else {
+                        _style.innerHTML = _cssText;
+                    }
+                    _appliedCss = _cssText;
+                }
+            }, 10);
+        }
+    };
+
+    if (source) {
+        Nonresponsive.parse(source);
+        Nonresponsive.apply();
+    }
+
+    Media.listen(Nonresponsive.apply);
+})(window /* [, css @media blocks] */);
